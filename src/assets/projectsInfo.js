44ce6b65d184d783feebf7185f65b7540bec45e1
Bill Sheng <billxsheng@gmail.com>
2018-09-17 08:43:51 -0400

    final project descriptions

--- src/assets/projectsInfo.js
+++ src/assets/projectsInfo.js
@@ -3,43 +3,43 @@ const ProjectsInfo = {
         name: 'Personal Website',
         description: 'My portfolio, made from scratch.',
         technologies: 'JavaScript (React), HTML, CSS, Bootstrap, Amazon Web Services (AWS), Photoshop',
-        about: 'This website acts as a showcase for personal projects and articles. I created all components and functionalities from scratch.'
+        about: 'This website acts as a showcase for personal projects and articles. All components and functionalities were created from scratch.'
     },
     Ryse: {
         name: 'Ryse',
         description: 'Networking application for ex-criminals.',
         technologies: 'JavaScript (Node.js, Express.js), MongoDB, HTML, CSS, Bootstrap',
-        about: 'This was the project my team created at PennApps 2018. One of the most prominent issues in today\'s society is the struggle for ex-convicts to find employment. Felons are forced to return to their old habits as they are consistently rejected when attempting to re-enter society. This leads to both economic and social issues. Ryse is a networking application made specifically to reduce the negative stigma associated with hiring ex-convicts. The app allows felons to showcase personal growth and initiative. They can also be endorsed by parole officers, doctors, and jail security in order to further promote personal changes and good behavior.'
+        about: 'This was the project my team created at PennApps 2018. One of the most prominent issues in today\'s society is the struggle for ex-convicts to find employment. Felons are forced to return to their old habits as they are consistently rejected when attempting to re-enter society. This is economically and socially detrimental. Ryse is a networking application made specifically to reduce the negative stigma associated with hiring ex-convicts. The app allows felons to showcase personal growth and initiative. They can also be endorsed by parole officers, doctors, and jail security in order to further promote personal changes and good behavior.'
     },
     PROG: {
         name: 'PROG',
         description: 'Accelerating group projects.',
         technologies: 'Microsoft Excel Developer Tools, Visual Basic for Applications (VBA)',
-        about: 'PROG is an excel-based product management tool aimed to increase efficiency and effectiveness during group projects. The app includes time management and scheduling tools that allow students to distribute the appropriate amount of time in the busy undergraduate lifestyle.'
+        about: 'PROG is an excel-based product management tool aimed to increase efficiency and effectiveness during group projects. The app includes time management and scheduling tools that allow a group of students to distribute the appropriate amount of time during the busy undergraduate lifestyle.'
     },
     CTC: {
         name: 'Canadian Tire',
         description: 'Improving IT productivity.',
         technologies: 'PowerShell, VMWare, Jira, Confluence',
-        about: 'During the summer of 2018, I interned at Canadian Tire Corporation in Downtown Toronto. Canadian Tire is a large Canadian retail company which sells a wide range of home, leisure, and finance products. As a technical specialist, my role was to help the IT team write scripts (mainly in PowerShell) that would simplify server configuration and virtualization. Overall, I had a lot of fun experiencing a real work environment and I can\'t wait to see what the future holds :)'
+        about: 'During the summer of 2018, I interned at Canadian Tire Corporation in Downtown Toronto. Canadian Tire is a large Canadian retail company which sells a wide range of home, leisure, and finance products. As a technical specialist, my role was to help the IT team write scripts (mainly in PowerShell) that would simplify server configuration and virtualization. Overall, I had a lot of fun experiencing a real work environment and I can\'t wait to see what the future holds. More coming soon :)'
     },
     Blitz: {
         name: 'Blitz',
         description: 'Redefining sports notifications.',
         technologies: 'JavaScript (Node.js, Express.js), MongoDB, HTML, CSS, Bootstrap',
-        about: 'Ever since I was a kid, I have loved to watch sports. In recent years, I have combined this interest with my passion for software. Blitz is an SMS-based NFL notification system that relies on text messages. This makes it extremely useful for sports fans without a cellular data plan. Users can sign up, select preferences, and recieve custom updates on their favorite NFL teams. The app features authentication, a variety of API\'s, and a complex algorithm which seamlessly sends the correct notification to the appropriate users.'
+        about: 'Ever since I was a kid, I have loved to watch sports. In recent years, I have combined this interest with my passion for software. Blitz is a NFL an notification system that relies on SMS text messages. This makes it extremely useful for sports fans without a cellular data plan. Users can sign up, select preferences, and recieve custom on-the-go updates on their favorite NFL teams. The app features authentication, a variety of API\'s, and a complex algorithm which seamlessly sends the correct notification to the appropriate users.'
     },
     Munkee: {
         name: 'Munkee',
         description: 'Online multiplayer game.',
         technologies: 'JavaScript (Node.js, Express.js, Socket.IO), MongoDB, HTML, CSS, Bootstrap',
-        about: 'Munkee is a full-stack online multiplayer game. It runs the classic game of PIG. I used Node.js and web sockets in order to implement online multiplayer capabilities.'
+        about: 'Munkee is a cross-platform online multiplayer game. It runs the classic game of PIG. I used Node.js and web sockets in order to implement the online multiplayer capabilities.'
     },
     Moodify: {
         name: 'Moodify',
         description: 'Adding AI to the job search.',
         technologies: 'Swift, Xcode, Objective-C, IBM Watson API',
-        about: 'Moodify is a IOS application which uses the IBM Watson text analyzer API. The API analyzes texts and reveals the moods and textures of the piece of text. This makes it useful for job-seekers when writing important documents and letters for employers.'
+        about: 'Moodify is a iOS application which uses the IBM Watson text analyzer API. The API analyzes texts and reveals deep moods and textures behind a piece of text. This makes it useful for job-seekers when writing important documents and letters for employers.'
     },
     vCuts: {
         name: 'VCuts',
