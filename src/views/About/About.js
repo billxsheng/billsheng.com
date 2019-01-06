61d4aff3f0ec3769d5fa55a93e6d007f8dadfc0c
Bill Sheng <billxsheng@gmail.com>
2019-01-05 22:03:09 -0500

    photos

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -31,9 +31,9 @@ class About extends Component {
 
                 <div styleName="content-div">
                     <div styleName="section-div-first" >
-                        {/* <div className="row">
-                            <Image  height="500px" path="about/team-picture.jpeg" />
-                        </div> */}
+                        <div className="row">
+                            <Image  height="450px" path="about/other/secondary.jpeg" />
+                        </div>
                         <div className="row">
                             <p2>
                                 {aboutInfo.general.description}
