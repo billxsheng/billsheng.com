7d7f4561aed3d8abc914b9fc81b6136887c4897b
Bill Sheng <billxsheng@gmail.com>
2019-01-07 21:37:51 -0500

    no periods

--- src/assets/projectsInfo.js
+++ src/assets/projectsInfo.js
@@ -2,18 +2,18 @@ const ProjectsInfo = {
     headings: {
         projects: {
             title: "Projects",
-            description: "A collection of projects & experiences."
+            description: "A collection of projects & experiences"
         },
         webMobile: {
             title: "Web & Mobile",
-            description: "Web/Mobile Development.",
+            description: "Web/Mobile Development",
             order: ['blitz', 'munkee', 'vcuts'],
             link: 'web-mobile'
         },
         ds: {
             title: "Data Science",
             link: 'data-science',
-            description: "Projects coming soon.",
+            description: "Projects coming soon",
             order: []
         },
         experience: {
@@ -24,7 +24,7 @@ const ProjectsInfo = {
         },
         hackathons: {
             title: "Hackathons",
-            description: "Projects built during hackathons.",
+            description: "Projects built during hackathons",
             order: ['ryse', 'moodify'],
             link: 'hackathons'
         },
@@ -32,7 +32,7 @@ const ProjectsInfo = {
     },
     ryse: {
         name: 'Ryse',
-        description: 'Networking application for ex-criminals.',
+        description: 'Networking application for ex-criminals',
         technologies: 'JavaScript (Node.js, Express.js), MongoDB, HTML, CSS, Bootstrap',
         about: "Coming Soon",       
         tag: "Hackathon"
@@ -41,35 +41,35 @@ const ProjectsInfo = {
     ctc: {
         name: 'Canadian Tire',
         link: 'canadian-tire',
-        description: 'Improving IT productivity.',
+        description: 'Improving IT productivity',
         technologies: 'PowerShell, VMWare, Jira, Confluence',
         tag: "Internship",
         about: "Coming Soon"    
     },
     blitz: {
         name: 'Blitz',
-        description: 'Redefining sports notifications.',
+        description: 'Redefining sports notifications',
         technologies: 'JavaScript (Node.js, Express.js), MongoDB, HTML, CSS, Bootstrap',
         tag: "Side Project",
         about: "Coming Soon"    
     },
     munkee: {
         name: 'Munkee',
-        description: 'Online multiplayer game.',
+        description: 'Online multiplayer game',
         technologies: 'JavaScript (Node.js, Express.js, Socket.IO), MongoDB, HTML, CSS, Bootstrap',
         tag: "Side Project",
         about: "Coming Soon"    
     },
     moodify: {
         name: 'Moodify',
-        description: 'Adding AI to the job search.',
+        description: 'Adding AI to the job search',
         technologies: 'Swift, Xcode, Objective-C, IBM Watson API',
         tag: "Hackathon",
         about: "Coming Soon"
     },
     vcuts: {
         name: 'VCuts',
-        description: 'Freelance barber website.',
+        description: 'Freelance barber website',
         technologies: 'JavaScript (Angular 2+, Node.js, jQuery), TypeScript, HTML, CSS, MongoDB, Amazon Web Services (AWS)',
         tag: "Freelance Project",
         about: "Coming Soon"
