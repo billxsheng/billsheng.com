038c3492b258e753cb57c47e0a221a5d8427991d
Bill Sheng <billxsheng@gmail.com>
2019-02-26 20:51:12 -0500

    bug fix

--- src/components/Button/MainButtonRow/MainButtonRow.js
+++ src/components/Button/MainButtonRow/MainButtonRow.js
@@ -6,17 +6,17 @@ import Icon from '../../Icon/Icon';
 import Links from '../../../assets/links';
 
 const MainButtonRow = (props) => (
-    <div className="row text-center row-fluid" styleName="btn-container">
-        <div className="span2 offset1">
+    <div className="row text-center" styleName="btn-container">
+        <div className="col-md-3">
             <Button resume="true" btnType="btnIcon" ><Icon iconName="far fa-file" iconType="iconSmall" /></Button>
         </div>
-        <div className="span2">
+        <div className="col-md-3">
             <Button newPage="false" link={Links.email} btnType="btnIcon" ><Icon iconName="far fa-envelope" iconType="iconSmall" /></Button>
         </div>
-        <div className="span2">
+        <div className="col-md-3">
             <Button link={Links.github} btnType="btnIcon" ><Icon iconName="fab fa-github" iconType="iconSmall" /></Button>
         </div>
-        <div className="span2">
+        <div className="col-md-3">
             <Button link={Links.linkedin} btnType="btnIcon" ><Icon iconName="fab fa-linkedin" iconType="iconSmall" /></Button>
         </div>
     </div>
