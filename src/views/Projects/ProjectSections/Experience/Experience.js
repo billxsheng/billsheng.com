00e8f8f640ca7e6400f737be5ad2a441b6f1a677
Bill Sheng <billxsheng@gmail.com>
2019-04-07 16:32:40 -0400

    update

--- src/views/Projects/ProjectSections/Experience/Experience.js
+++ src/views/Projects/ProjectSections/Experience/Experience.js
@@ -61,13 +61,13 @@ class Experience extends Component {
                         </p>
                 </span>
                 <div className="row">
-                    <ProjectTile to="/projects/experience/opentext" singleTile = "true" imageHeight="400px" imageName="projects/pweb.png" tag={projectsInfo.opentext.tag} title={projectsInfo.opentext.name} shortDesc={projectsInfo.opentext.description} />
+                    <ProjectTile to="/projects/experience/opentext" imageName = "projects/ot.png" imageHeight="400px"  tag={projectsInfo.opentext.tag} title={projectsInfo.opentext.name} shortDesc={projectsInfo.opentext.description} />
                 </div>
                 <div className="row">
-                    <ProjectTile to="/projects/experience/freelance" singleTile = "true" tag={projectsInfo.freelance.tag} title={projectsInfo.freelance.name} shortDesc={projectsInfo.freelance.description} />
+                    <ProjectTile to="/projects/experience/freelance" imageHeight="400px" reverse="true" imageName="projects/freelance.png" tag={projectsInfo.freelance.tag} title={projectsInfo.freelance.name} shortDesc={projectsInfo.freelance.description} />
                 </div>
                 <div className="row">
-                    <ProjectTile to="/projects/experience/canadian-tire" top="25px" imageHeight="400px" imageName="projects/ctc.png" tag={projectsInfo.ctc.tag}title={projectsInfo.ctc.name} shortDesc={projectsInfo.ctc.description}/>
+                    <ProjectTile to="/projects/experience/canadian-tire"  top="25px" imageHeight="400px" imageName="projects/ctc.png" tag={projectsInfo.ctc.tag}title={projectsInfo.ctc.name} shortDesc={projectsInfo.ctc.description}/>
                 </div>
             </div>
         )
