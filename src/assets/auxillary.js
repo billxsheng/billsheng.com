d34433b003516cc17b625453e292c83d6d617caa
Bill Sheng <billxsheng@gmail.com>
2019-04-07 23:52:51 -0400

    update temp branch

---
+++ src/assets/auxillary.js
@@ -0,0 +1,5 @@
+const auxillary = {
+    footer: "Created from scratch by Bill Sheng"
+}
+
+export default auxillary;
---
+++ src/assets/galleryInfo.js
@@ -0,0 +1,25 @@
+const GalleryInfo = {
+    headings: {
+        gallery: {
+            title: "Gallery",
+            description: "Ideas, thoughts, and takeaways"
+        }
+    },
+    snl: {
+        title: 'SNL',
+        date: "Coming Soon"
+        // description: 'The dominance of the FBS',
+    },
+    graphql: {
+        title: 'GraphQL',
+        date: "Coming Soon"
+        // description: 'API\'s Reimagined',
+    },
+    year1: {
+        title: '1st Year',
+        date: "Coming Soon"
+        // description: 'Thoughts and Takeaways',
+    }
+}
+
+export default GalleryInfo;
