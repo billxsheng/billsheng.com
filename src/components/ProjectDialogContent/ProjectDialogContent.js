cab6edb592f8c8859d98d51befe36c83475120b9
billxsheng <billxsheng@gmail.com>
2019-04-09 15:44:11 -0400

    bug fixes

--- src/components/ProjectDialogContent/ProjectDialogContent.js
+++ src/components/ProjectDialogContent/ProjectDialogContent.js
@@ -40,7 +40,7 @@ class ProjectModal extends Component {
                 <p>{project.tag}</p>
             </div>
             <img src={require('../../assets/images/projects/blitz.png')} />
-            <h3>Summary</h3>
+            <h4>Summary</h4>
             <p2>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
               Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, 
               nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
