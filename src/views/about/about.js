79e96473b38ae1cb3b7a28eae9a3f8f484c166dc
Bill Sheng <bill.sheng@shopify.com>
2021-04-27 21:59:05 -0400

    About page responsive adjustments

--- src/views/about/about.js
+++ src/views/about/about.js
@@ -7,7 +7,7 @@ import styles from "./about.css";
 import Image from 'react-bootstrap/Image';
 import Tile from '../../components/tile/tile';
 import { Link } from 'react-router-dom';
-import { ArrowLeftSquareFill } from 'react-bootstrap-icons';
+import { ArrowDown, ArrowDownCircleFill, ArrowLeftSquareFill } from 'react-bootstrap-icons';
 import projects from '../../assets/projects'
 
 
@@ -18,48 +18,56 @@ class About extends Component {
 
     render() {
         return (
-            <Container styleName="about-container">
-                <Row styleName="back-row">
-                    <Link styleName="back-arrow" to="/"><ArrowLeftSquareFill /></Link>
-                </Row>
-                <Row styleName="row">
-                    <Col md={8} styleName="col-text">
-                        <Row>
-                            <div styleName="content-wrapper">
-                                <p><b>Hey, I'm Bill!</b> I'm a product-driven, people-focused engineer with extensive expertise designing, developing, and
+            <div>
+                <Container styleName="summary-container">
+                    <Row styleName="back-row">
+                        <Link styleName="back-arrow" to="/"><ArrowLeftSquareFill /></Link>
+                    </Row>
+                    <Row styleName="row">
+                        <Col md={8} styleName="col-text">
+                            <Row>
+                                <div styleName="content-wrapper">
+                                    <p><b>Hey, I'm Bill!</b> I'm a product-driven, people-focused engineer with extensive expertise designing, developing, and
                                 shipping software solutions through critical thinking, active collaboration, and best practices.</p>
-                                <p>I've worked as an engineering intern at five companies, including
+                                    <p>I've worked as an engineering intern at five companies, including
                                  <a target="_blank" styleName="text-link-shopify" href="https://shopify.ca">&nbsp;Shopify&nbsp;</a>
                                   and
                                   <a target="_blank" styleName="text-link-nvidia" href="https://fortune.com/fortune500/">&nbsp;NVIDIA</a>
                                   . I'm currently in my 4A term of Management Engineering at the University of Waterloo.
                                   </p>
-                                <p>In my spare time, I like to exercise, watch sports, play video games, and read.</p>
-                            </div>
-                        </Row>
-                    </Col>
-                    <Col md={4} styleName="col-image">
-                        <Row styleName="title">
-                            <Image fluid styleName="image" src={require(`../../assets/images/home-pic.jpg`)} />
-                        </Row>
-                    </Col>
-
-                </Row>
-
-                <Row styleName="tile-row">
-                    <Col styleName="col-text">
-                        <h2>Experiences</h2>
-                    </Col>
-                </Row>
-                {projects.experiences.map((exp) => {
-                    console.log(exp.name)
-                    return (
-                    <Row styleName="tile-row">
-                        <Tile name={exp.name} position={exp.position} duration={exp.duration} image_name={exp.image_name}/>
+                                    <p>In my spare time, I like to exercise, watch sports, play video games, and read.</p>
+                                </div>
+                            </Row>
+                        </Col>
+                        <Col md={4} styleName="col-image">
+                            <Row>
+                                <Image fluid styleName="image" src={require(`../../assets/images/home-pic.jpg`)} />
+                            </Row>
+                        </Col>
                     </Row>
-                    )
-                })}
-            </Container>
+                    <Row styleName="icon-row">
+                        <ArrowDownCircleFill />
+                    </Row>
+                </Container>
+
+                <Container styleName="exp-container">
+                    <Row styleName="tile-row-header">
+                        <Col styleName="col-text">
+                            <h2>Experiences</h2>
+                        </Col>
+                    </Row>
+                    {projects.experiences.map((exp) => {
+                        console.log(exp.name)
+                        return (
+                            <Row styleName="tile-row">
+                                <Tile name={exp.name} position={exp.position} duration={exp.duration} image_name={exp.image_name} />
+                            </Row>
+                        )
+                    })}
+                </Container>
+
+            </div>
+
         )
     }
 };
